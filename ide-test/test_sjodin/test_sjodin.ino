9311880982ad61523af5cb88e5c21c71559cc7df
Francisco Paez <franpaez@gmail.com>
2013-10-21 15:40:00 -0300

    Wcrt.

--- ide-test/test_sjodin/test_sjodin.ino
+++ ide-test/test_sjodin/test_sjodin.ino
@@ -9,74 +9,247 @@ struct task_t {
   int wcrt;
 };
 
-task_t str[] = {{1,1,132,132,0}, 
-                {2,1,139,139,0}, 
-                {3,1,145,145,0},
-                {4,1,194,194,0},
-                {5,3,320,320,0},
-                {6,1,521,521,0},
-                {7,7,639,639,0},
-                {8,10,730,730,0},
-                {9,13,851,851,0},
-                {10,24,910,910,0}
-            };
+task_t str[10];
+
 int num_task = 0;
+unsigned long t_start, t_end;
 
 boolean schedulable;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  while (!Serial)
-    ;
+  handshake();
 }
 
 void loop() {
-  while (Serial.available() <= 0)
-    ;
-  
   // Numero de tareas
   if (Serial.available() > 0) {
     num_task = Serial.parseInt();
+    Serial.println(num_task);
+    delay(10);
+    
+    for (int i = 0; i < num_task; i++) {
+      str[i].id = i + 1;
+      str[i].c = Serial.parseInt();
+      str[i].t = Serial.parseInt();
+      str[i].d = Serial.parseInt();
+      str[i].wcrt = 0;
+    }
+    
+    Serial.println("rta2");
+    t_start = micros();
+    rta2_wcrt();
+    t_end = micros();    
+    if (schedulable) {
+      Serial.println("True");
+    } else {
+      Serial.println("False");
+    }    
+    for (int i = 0; i < num_task; i++) {
+      Serial.println(str[i].wcrt, DEC);
+    }    
+    Serial.println(t_end - t_start);
+
+
+    Serial.println("rta3");    
+    t_start = micros();
+    rta3_wcrt();
+    t_end = micros();    
+    if (schedulable) {
+      Serial.println("True");
+    } else {
+      Serial.println("False");
+    }    
+    for (int i = 0; i < num_task; i++) {
+      Serial.println(str[i].wcrt, DEC);
+    }    
+    Serial.println(t_end - t_start);
+
+    Serial.println("sjodin");
+    t_start = micros();
+    sjodin_wcrt();
+    t_end = micros();    
+    if (schedulable) {
+      Serial.println("True");
+    } else {
+      Serial.println("False");
+    }
+    for (int i = 0; i < num_task; i++) {
+      Serial.println(str[i].wcrt, DEC);
+    }    
+    Serial.println(t_end - t_start);
   }
   
-  Serial.print("Nro. tareas: ");
-  Serial.println(num_task, DEC);
-  
-  for (int i = 0; i < num_task; i++) {
-    str[i].id = i + 1;
-    while (Serial.available() <= 0)
-      ;
-    str[i].c = Serial.parseInt();
-    while (Serial.available() <= 0)
-      ;
-    str[i].t = Serial.parseInt();
-    while (Serial.available() <= 0)
-      ;
-    str[i].d = Serial.parseInt();
-    str[i].wcrt = 0;
-    Serial.println("Tarea 1: ");
-    Serial.println(str[i].c, DEC);
-    Serial.println(str[i].t, DEC);
-    Serial.println(str[i].d, DEC);    
-  }
-  
-  Serial.println("Calculando por Sjodin...");
-  sjodin_wcrt();
-  
-  if (schedulable) {
-    Serial.println("Planificable!");
-  } else {
-    Serial.println("No planificable!");
-  }
-  
-  for (int i = 0; i < num_task; i++) {
-    Serial.print("Tarea ");
-    Serial.print(i, DEC);
-    Serial.print(": ");
-    Serial.println(str[i].wcrt, DEC);
+  handshake();
+}
+
+void handshake() {
+  while (Serial.available() <= 0) {
+    Serial.write('A');
+    delay(300);
   }
 }
+
+void rta3_wcrt() {
+  int aes[num_task];
+  int bes[num_task];
+  int tr;
+  int w;
+  int j;
+  int min_b;
+  
+  for (int i = 0; i < num_task; i++) {
+    aes[i] = 0;
+    bes[i] = 0;
+  }
+  
+  schedulable = true;
+  
+  int t = str[0].c;
+  str[0].wcrt = str[0].c;
+  
+  for (int i = 1; i < num_task; i++) {
+    t = t + str[i].c;
+    tr = t;
+    
+    aes[i] = str[i].c;
+    bes[i] = str[i].t;
+    
+    while (true) {
+      w = 0;
+      j = i - 1;
+      
+      min_b = str[i].t;
+      
+      while (j >= 0) {
+        if (bes[j] < min_b) {
+          min_b = bes[j];
+        }
+        
+        // no calculamos el termino A si no salta
+        if (tr <= bes[j]) {
+          w = w + aes[j];
+          j = j - 1;
+          continue;
+        }
+        
+        // calculamos A
+        int c_j = str[j].c;
+        int t_j = str[j].t;        
+        int a_t = (int)ceil( ((double)tr) / ((double)t_j) );
+        int a = a_t * c_j;
+        
+        // ya sabemos que A salta, computamos la diferencia
+        tr = tr + a - aes[j];
+        if (tr > str[i].d) {
+          schedulable = false;
+          return;
+        }
+        
+        aes[j] = a;
+        bes[j] = a_t * str[j].t;
+        
+        if (bes[j] < min_b) {
+          min_b = bes[j];
+        }
+        
+        // vuelve para atras si el nuevo t es mayor que el
+        // limite de salto del termino a previo
+        if ((j < i) && (tr > bes[j + 1])) {
+          j = j + 1;
+          w = w - aes[j];
+          continue;
+        }
+        
+        w = w + a;
+        j = j - 1;
+      }
+      
+      w = w + str[i].c;
+      
+      if (t == w) {
+        aes[i] = str[i].c;
+        bes[i] = str[i].t;
+        break;
+      }
+      
+      if (tr > w) {
+        t = tr;
+      } else {
+        t = w;
+      }
+      
+      if (t <= min_b) {
+        aes[i] = str[i].c;
+        bes[i] = str[i].t;
+        break;
+      }
+      
+      tr = t;
+    }
+    
+    str[i].wcrt = t;
+  }
+}   
+
+void rta2_wcrt() {
+  int aes[num_task];
+  int tr = 0;
+  int w = 0;
+  
+  for (int i = 0; i < num_task; i++) {
+    aes[i] = 0;
+  }  
+  
+  schedulable = true;
+  
+  int t = str[0].c;
+  str[0].wcrt = str[0].c;
+  
+  for (int i = 1; i < num_task; i++) {
+    t = t + str[i].c;
+    tr = t;
+    
+    while (true) {
+      w = 0;
+      
+      for (int j = 0; j < i; j++) {
+        int c_j = str[j].c;
+        int t_j = str[j].t;        
+        int a = (int)ceil( ((double)tr) / ((double)t_j) );
+        a = a * c_j;
+        if (aes[j] > 0) {
+          tr = tr + a - aes[j];
+        }
+        if (tr > str[i].d) {
+          t = tr;
+          schedulable = false;
+          return;
+        }
+        aes[j] = a;
+        w = w + a;
+      }
+      
+      w = w + str[i].c;
+      
+      if (t == w) {
+        aes[i] = str[i].c;
+        break;
+      }
+      
+      if (tr > w) {
+        t = tr;
+      } else {
+        t = w;
+      }
+      
+      tr = t;
+    }
+    
+    str[i].wcrt = t;
+  }
+}   
 
 void sjodin_wcrt() {
   int r = 0;
@@ -115,7 +288,7 @@ void sjodin_wcrt() {
     }
     
     if (!schedulable) {
-      //break;
+      break;
     }
     
     str[i].wcrt = r;     
