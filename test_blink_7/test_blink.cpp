7610bc59ca22c22c5f64d4982abe55666440d4be
Francisco Paez <franpaez@gmail.com>
2014-06-02 14:20:13 -0300

    Mueve lógica a archivo externo.

---
+++ test_blink_7/test_blink.cpp
@@ -0,0 +1,37 @@
+#include "test_blink.h"
+
+#define portPIOC06     ( 1UL << 6UL )
+
+// Mediante volatile especifica que usamos el puntero para acceder a
+// dispositivos mapeados en la memoria.
+#define portPIOC_PER   ( * ( ( volatile int * ) 0x400E1200 ) )
+#define portPIOC_OER   ( * ( ( volatile int * ) 0x400E1210 ) )
+#define portPIOC_SODR  ( * ( ( volatile int * ) 0x400E1230 ) )
+#define portPIOC_CODR  ( * ( ( volatile int * ) 0x400E1234 ) )
+
+// Prototipos
+void busy_wait(void);
+
+void my_setup() {
+    portPIOC_PER = portPIOC06; 
+    portPIOC_OER = portPIOC06;
+}
+
+void my_loop() {
+    // Apaga los LEDs
+    portPIOC_SODR = portPIOC06; 
+    busy_wait();
+
+    // Prende los LEDs
+    portPIOC_CODR = portPIOC06;
+    busy_wait();
+}
+
+void busy_wait() {
+    int i;
+    for(i=0;i<0x1FFFFF;i++){
+        asm("nop");
+    }
+}
+
+
--- test_blink_7/test_blink_7.ino
+++ test_blink_7/test_blink_7.ino
@@ -1,30 +1,9 @@
-// 
-#define portPIOC06     ( 1UL << 6UL )
-#define portPIOC07     ( 1UL << 7UL )
-#define portPIOC08     ( 1UL << 8UL )
-
-// Mediante volatile podemos especificar que usamos el puntero para acceder a 
-// dispositivos mapeados en la memoria.
-#define portPIOC_PER   ( * ( ( volatile uint32_t * ) 0x400E1200 ) )
-#define portPIOC_OER   ( * ( ( volatile uint32_t * ) 0x400E1210 ) )
-#define portPIOC_SODR  ( * ( ( volatile uint32_t * ) 0x400E1230 ) )
-#define portPIOC_CODR  ( * ( ( volatile uint32_t * ) 0x400E1234 ) )
+#include "test_blink.h"
 
 void setup() {  
-  portPIOC_PER = portPIOC06 | portPIOC07 | portPIOC08;
-  portPIOC_OER = portPIOC06 | portPIOC07 | portPIOC08;
+    my_setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  portPIOC_SODR = portPIOC06 | portPIOC07 | portPIOC08;
-  for(int i=0;i<0x1FFFFF;i++){
-    asm("nop");
-  }
- 
-  portPIOC_CODR = portPIOC06 | portPIOC07 | portPIOC08;
-  for(int i=0;i<0x1FFFFF;i++){
-    asm("nop");
-  }
-
+    my_loop();
 }
