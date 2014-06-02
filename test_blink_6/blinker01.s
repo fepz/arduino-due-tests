380d112fab6eb97f9220931947b26cfe7beb874c
Francisco Paez <franpaez@gmail.com>
2014-06-02 14:19:40 -0300

    Simplifica funciones.

--- test_blink_6/blinker01.s
+++ test_blink_6/blinker01.s
@@ -1,43 +1,33 @@
 .cpu cortex-m3
 .thumb
 
-/* Vector table definition */
-.word 0x20088000 /* stack top address */
-.word _start /* 1 Reset */
-.word hang /* 2 NMI */
-.word hang /* 3 HardFault */
-.word hang /* 4 MemManage */
-.word hang /* 5 BusFault */
-.word hang /* 6 UsageFault */
-.word hang /* 7 RESERVED */
-.word hang /* 8 RESERVED */
-.word hang /* 9 RESERVED*/
-.word hang /* 10 RESERVED */
-.word hang /* 11 SVCall */
-.word hang /* 12 Debug Monitor */
-.word hang /* 13 RESERVED */
-.word hang /* 14 PendSV */
-.word hang /* 15 SysTick */
+/* Vector de interrupciones */
+.word 0x20088000    /* Dirección base de la pila */
+.word _start        /* 1 Entry point */
+.word hang          /* 2 NMI */
+.word hang          /* 3 HardFault */
+.word hang          /* 4 MemManage */
+.word hang          /* 5 BusFault */
+.word hang          /* 6 UsageFault */
+.word hang          /* 7 reservado */
+.word hang          /* 8 reservado */
+.word hang          /* 9 reservado */
+.word hang          /* 10 RESERVED */
+.word hang          /* 11 SVCall */
+.word hang          /* 12 Debug Monitor */
+.word hang          /* 13 reservado */
+.word hang          /* 14 PendSV */
+.word hang          /* 15 SysTick */
 
+/* Ciclo infinito */
 .thumb_func
 hang:
     b .
 
+/* Funcion principal */
 .thumb_func
 .global main
 _start:
-    /* Set the pins direction as output */
-    bl      set_gpio_dir
-
-    __loop:
-        bl      clear_leds
-        bl      delay
-        bl      set_leds
-        bl      delay
-        b       __loop
- 
-.thumb_func
-set_gpio_dir:
     /* El LED rojo de la placa de desarrollo se encuentra conectado
         al PIN digital 38 de Arduino. Este corresponde al PIN PC6 de
         el SAM3X */
@@ -47,6 +37,20 @@ set_gpio_dir:
     movw    r3, #0x1200
     movt    r3, #0x400E
 
+    bl      set_gpio_dir    // Configura los PINs a utilizar
+
+    __loop:
+        bl      clear_leds  // Apaga los LEDs
+        bl      delay       // Espera activa
+        bl      set_leds    // Prende los LEDs
+        bl      delay       // Espera activa
+        b       __loop      // Repetir
+
+    b .
+ 
+/* Configura los PINs de salida a utilizar */
+.thumb_func
+set_gpio_dir:
     /* Habilito el GPIO PIOC06 (LED rojo de la placa de deasarrollo) */
     /* PIO_PER para PIOC en 0x400E1200 */
     /* Habilitar PIOC06: 0x00000040 */
@@ -61,37 +65,27 @@ set_gpio_dir:
  
 .thumb_func
 set_leds:
-    mov     r2, #1
-    lsl     r2, #6
     /* Sets the data to be driven on the I/O line */
     /* PIO_SODR para PIOC en 0x400E1230 */
-    movw    r3, #0x1230
-    movt    r3, #0x400E
     /* Habilitar PIOC06: 0x00000040 */
-    str     r2, [r3, #0]
-
+    str     r2, [r3, #0x30]
     bx      lr 
  
 .thumb_func
 clear_leds:
-    mov     r2, #1
-    lsl     r2, #6
     /* Clears the data to be driven on the I/O line */
     /* PIO_CODR para PIOC en 0x400E1234 */
-    movw    r3, #0x1234
-    movt    r3, #0x400E
     /* Habilitar PIOC06: 0x00000040 */
-    str     r2, [r3, #0]
-
+    str     r2, [r3, #0x34]
     bx      lr 
  
 .thumb_func
 delay:
-    movw    r3, #0xFFFF
-    movt    r3, #0x000F
+    movw    r4, #0xFFFF
+    movt    r4, #0x000F
     __delay_loop:
-        cbz     r3, __delay_exit
-        sub     r3, r3, #1
+        cbz     r4, __delay_exit
+        sub     r4, r4, #1
         b       __delay_loop
     __delay_exit:
         bx      lr
